0aa2bf5ff65b418643e56f170dd831acc0a2d306
Rhys Powell <rhys@rpowell.me>
2016-10-28 21:27:41 +1100

    Organise code

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import Filter from './Filter'
-import App from './App'
+import Filter from '../Filter'
+import App from '../components/App'
 
 class AppContainer extends Component {
   constructor () {
