b3c67a9eb8dc61911285f076dcaee653670918a3
Rhys Powell <rhys@rpowell.me>
2016-11-10 10:12:17 +1100

    Remove some old tests, add some new ones

--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -1,11 +1,13 @@
 /* eslint-env jest */
 import React from 'react'
 import renderer from 'react-test-renderer'
-import TodoInput from './TodoInput'
+import Footer from './Footer'
+
+const filters = ['one', 'two', 'three']
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <TodoInput value='Test Item' />
+    <Footer filters={filters} activeFilter='one' />
   ).toJSON()
   expect(tree).toMatchSnapshot()
 })
