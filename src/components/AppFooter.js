e70088290694a2255d154fba668b55bbc2203da1
Rhys Powell <rhys@rpowell.me>
2018-12-11 10:56:57 +1100

    update footer link

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,7 +5,7 @@ const AppFooter = () => (
   <footer className='app-footer'>
     <span>Made with a little knowledge, and a lot of <span role="img" aria-label="heart">❤️</span> by <a href='https://twitter.com/rhysforyou'>Rhys Powell</a></span>
     <br />
-    <span><a href='https://github.com/rpowelll/react-todo'>View on GitHub</a></span>
+    <span><a href='https://github.com/rhysforyou/react-todo'>View on GitHub</a></span>
   </footer>
 )
 
